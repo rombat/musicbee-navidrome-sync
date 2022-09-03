2774dfd2731c7207dbd0aee7efdb35363ba02709
Romain Batigne <roro@axeptio.eu>
2022-09-03 10:58:41 +0200

    fix: progressBar length for albums and artists

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ program
   .option('-u, --user <user_name>', 'choose Navidrome username (by default if not used, the first user will be used)')
   .option('-f, --first', 'run sync for the first time: add MB playcount to ND playcount')
   .option('-v, --verbose', 'verbose debugging')
+  .option('--csv <path>', 'MusicBee CSV source file path')
+  .option('--db <path>', 'Navidrome SQLITE DB source file path')
   .action(fullSync);
 
 program
