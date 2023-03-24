64e5007de9cb90cd9dbf336212d5df9d664ec0a0
Romain Batigne <roro@axeptio.eu>
2023-03-24 18:35:37 +0100

    refactor: command line options

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const runAction = async (options, command) => {
   await synchronizer.run(command._name);
 };
 
+const commandLinesOptions = {
+  csv: {
+    flags: '--csv <path>',
+    description: 'MusicBee CSV source file path. Default: MusicBee_Export.csv, in the same folder as MBNDS',
+    defaultValue: 'MusicBee_Export.csv'
+  },
+  db: {
+    flags: '--db <path>',
+    description: 'Navidrome SQLITE .db source file path. Default: navidrome.db, in the same folder as MBNDS',
+    defaultValue: 'navidrome.db'
+  },
+  user: {
+    flags: '-u, --user <user_name>',
+    description: 'choose Navidrome username (by default if not used, the first user will be used)'
+  },
+  verbose: {
+    flags: '--verbose',
+    description: 'verbose debugging'
+  }
+};
+
 program
   .name('musicbee-navidrome-sync')
   .description(
@@ -17,43 +38,27 @@ program
 program
   .command('fullSync')
   .description('sync playcounts, track ratings, loved tracks and last played from MusicBee DB to Navidrome DB')
-  .option('-u, --user <user_name>', 'choose Navidrome username (by default if not used, the first user will be used)')
+  .option(commandLinesOptions.user.flags, commandLinesOptions.user.description)
   .option('-f, --first', 'run sync for the first time: add MB playcount to ND playcount')
-  .option('--verbose', 'verbose debugging')
-  .option(
-    '--csv <path>',
-    'MusicBee CSV source file path. Default: MusicBee_Export.csv, in the same folder as MBNDS',
-    'MusicBee_Export.csv'
-  )
-  .option(
-    '--db <path>',
-    'Navidrome SQLITE .db source file path. Default: navidrome.db, in the same folder as MBNDS',
-    'navidrome.db'
-  )
+  .option(commandLinesOptions.verbose.flags, commandLinesOptions.verbose.description)
+  .option(commandLinesOptions.csv.flags, commandLinesOptions.description, commandLinesOptions.defaultValue)
+  .option(commandLinesOptions.db.flags, commandLinesOptions.db.description, commandLinesOptions.db.defaultValue)
   .action(runAction);
 
 program
   .command('albumsSync')
   .description('update all albums playcounts and ratings based on existing Navidrome DB')
-  .option('-u, --user <user_name>', 'choose Navidrome username (by default if not used, the first user will be used)')
-  .option('--verbose', 'verbose debugging')
-  .option(
-    '--db <path>',
-    'Navidrome SQLITE .db source file path. Default: navidrome.db, in the same folder as MBNDS',
-    'navidrome.db'
-  )
+  .option(commandLinesOptions.user.flags, commandLinesOptions.user.description)
+  .option(commandLinesOptions.verbose.flags, commandLinesOptions.verbose.description)
+  .option(commandLinesOptions.db.flags, commandLinesOptions.db.description, commandLinesOptions.db.defaultValue)
   .action(runAction);
 
 program
   .command('artistsSync')
   .description('update all artists playcounts and ratings based on existing Navidrome DB')
-  .option('-u, --user <user_name>', 'choose Navidrome username (by default if not used, the first user will be used)')
-  .option('--verbose', 'verbose debugging')
-  .option(
-    '--db <path>',
-    'Navidrome SQLITE .db source file path. Default: navidrome.db, in the same folder as MBNDS',
-    'navidrome.db'
-  )
+  .option(commandLinesOptions.user.flags, commandLinesOptions.user.description)
+  .option(commandLinesOptions.verbose.flags, commandLinesOptions.verbose.description)
+  .option(commandLinesOptions.db.flags, commandLinesOptions.db.description, commandLinesOptions.db.defaultValue)
   .action(runAction);
 
 program.parse();
